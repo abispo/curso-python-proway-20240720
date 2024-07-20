5cb27ff13d5b003cd2b9eca802b4c3538955fcf3
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-07-20 11:06:30 -0300

    Arquivos aula 01 python fundamentos

---
+++ modulo01-fundamentos/20240720-aula01/prog03.py
@@ -0,0 +1,12 @@
+# Tipos de dados em Python
+# Tipos numéricos (int, float, complex)
+
+if __name__ == "__main__":
+
+    numero1: int = int(input("Informe o primeiro numero: "))
+    numero2 = int(input("Informe o segundo numero: "))
+
+    print(f"Resultado da soma: {numero1 + numero2}")
+
+    valor = float(input("Informe a porcentagem: "))
+    print(valor)
