a5ef45e32c14f10ecf19c31cfed3c5598ecaf444
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-07-20 11:15:55 -0300

    Arquivos aula 01 python fundamentos

---
+++ modulo01-fundamentos/20240720-aula01/prog04.py
@@ -0,0 +1,11 @@
+# Tipos de dados em Python
+# bool (True, False)
+
+if __name__ == "__main__":
+    
+    print(5 > 2)
+    print(10 <= 20)
+    print(bool(0))
+
+    valor = True
+    negacao = False
