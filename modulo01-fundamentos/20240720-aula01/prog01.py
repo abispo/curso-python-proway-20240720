08fff956b44aef6db46f4df7bdba121b197c4332
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-07-20 09:21:56 -0300

    Arquivos aula 01 python fundamentos

---
+++ modulo01-fundamentos/20240720-aula01/prog01.py
@@ -0,0 +1,11 @@
+# Isso é um comentário
+
+"""
+No Python, não existe um caractere específico para comentários multi linha. Para isso, utilizamos a string multilinha.
+
+"""
+
+# A primeira linha, é uma expressão em Python
+# Uma expressão consiste de um conjunto de operadores e operandos, que geram um valor.
+if __name__ == "__main__":
+    print("Olá mundo")
