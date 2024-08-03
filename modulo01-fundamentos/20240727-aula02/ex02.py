27e4aef44b8f28b72fd6b7d447506621f312b6ad
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-08-03 08:39:40 -0300

    Arquivos aula 03 python fundamentos

---
+++ modulo01-fundamentos/20240727-aula02/ex02.py
@@ -0,0 +1,20 @@
+# 2. Escreva um programa que receba números pelo terminal. Se o usuário digitar o número 0, o programa para de receber números pelo terminal e retorna uma lista dos quadrados desses números.
+
+if __name__ == "__main__":
+    
+
+    lista_numeros = []
+
+    while True:
+        
+        numero = int(input("Informe um número: "))
+
+        if numero == 0:
+            print("Informado número 0.")
+            break
+
+        lista_numeros.append(numero)
+
+    lista_quadrados = [numero * numero for numero in lista_numeros]
+
+    print(f"Lista dos quadrados: {lista_quadrados}")
