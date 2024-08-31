aa95269577daee8948e6f1e2b071221ff0313989
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-08-31 12:10:58 -0300

    Arquivos aula 03 python com banco de dados

--- modulo02-python-com-banco-de-dados/20240831-aula03/prog01.py
+++ modulo02-python-com-banco-de-dados/20240831-aula03/prog01.py
@@ -10,12 +10,18 @@ Classes, Métodos, Atributos e Objetos
 # UPPER_CASE    -> BIT = 1
 
 class Pokemon:
+    # O método __init__ é o método inicializador da classe, ou seja, nesse método criamos os atributos da classe e também podemos chamar os métodos. No caso abaixo, o método __init__ irá receber 3 valores: nome, tipo, hp
+    # No caso dos métodos em uma classe Python, o primeiro parâmetro deve ser obrigatoriamente o self, pois é uma referência ao próprio objeto instanciado.
     def __init__(self, nome: str, tipo: str, hp: int) -> None:
+        # Abaixo estamos inicializando 3 atributos: _nome, _tipo e _hp. Como no Python não existe uma palavra reservada que faça um atributo ser privado, os atributos que queremos que sejam entendidos como privados criamos com um underline na frente.
+        # Abaixo os atributos estão recebendo os valores passados na criação do objeto
         self._nome = nome
         self._tipo = tipo
-        self._hp = hp
+        self._hp = hp   # vitalidade
 
     def get_nome(self):
+        # Obrigatoriamente, todos os métodos dentro de uma classe Python devem receber no mínimo o parâmetro self, que é uma referência ao próprio objeto que está sendo criado. Você pode verificar isso utilizando o método built-in id. Por exemplo: id(self) e depois passando o objeto criado para o método: id(pikachu)
+        # Caso o método seja do tipo classmethod ou staticmethod, nós não passamos o parâmetro self
         return self._nome
     
     def get_tipo(self):
@@ -24,10 +30,12 @@ class Pokemon:
     def set_tipo(self, novo_tipo):
         self._tipo = novo_tipo
 
+    # Abaixo utilizamos o decorator property (decorators são funções que extendem o comportamento de outras funções). Nesse caso, o método hp vai se comportar como uma propriedade hp. Ou seja, ao invés de fazermos objeto.hp(), fazemos objeto.hp.
     @property
     def hp(self):
         return self._hp
     
+    # Depois que aplicamos o decorator property, podemos criar um outro método que irá alterar o valor do atributo. Novamente, essse método irá se comportar como um atributo da classe. Ou seja, ao invés de fazermos objeto.hp(10), fazemos objeto.hp = 10
     @hp.setter
     def hp(self, novo_hp):
         self._hp = novo_hp
@@ -49,3 +57,51 @@ if __name__ == "__main__":
         tipo="Elétrico",
         hp=60
     )
+
+    charmander = Pokemon(
+        nome="Charmander",
+        tipo="Fogo",
+        hp=120
+    )
+
+    # session.query(Pokemon).all()
+
+    [pikachu, charmander]
+
+    print(pikachu.get_tipo())
+    pikachu.set_tipo("Aquático")
+    print(pikachu.get_tipo())
+
+    print(pikachu.hp)
+    pikachu.hp = 50
+    print(pikachu.hp)
+
+
+class Celular:
+    def __init__(self) -> None:
+        self._fabricante = "Samsung"
+
+
+class Usuario:
+    id: int
+    email: str
+    senha: str
+
+class Celular:
+    def __init__(self) -> None:
+        self._ligado = False
+
+    def ligar(self):
+        self._ligado = True
+
+    def desligar(self):
+        self._ligado = False
+
+    def esta_ligado(self):
+        return self._ligado
+
+samsung = Celular()
+print("Celular instanciado")
+print("Ligado" if samsung.esta_ligado() else "Desligado")
+samsung.ligar()
+print("Ligado" if samsung.esta_ligado() else "Desligado")
