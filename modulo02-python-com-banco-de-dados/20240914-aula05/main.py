f4a7c47e902b2e400131722379e36b8de7731a98
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-09-14 11:05:31 -0300

    Arquivos aula 05 python com banco de dadoa

--- modulo02-python-com-banco-de-dados/20240914-aula05/main.py
+++ modulo02-python-com-banco-de-dados/20240914-aula05/main.py
@@ -1,6 +1,16 @@
 
 from config import Base, connection
+from mensagens import MENU_GERAL
 from models import Usuario, Perfil
 
 if __name__ == "__main__":
     Base.metadata.create_all(connection)
+
+    while True:
+        print(MENU_GERAL)
+        opcao = int(input("Informe a opção: "))
+
+        match opcao:
+            case 0:
+                print("Saindo...")
+                break
--- modulo02-python-com-banco-de-dados/20240914-aula05/usuarios.py
+++ modulo02-python-com-banco-de-dados/20240914-aula05/usuarios.py
@@ -1,7 +1,18 @@
 
 from config import session
+from mensagens import MENU_USUARIOS
 from models import Usuario, Perfil
 
+def gerenciar_usuarios():
+    
+    while True:
+        print(MENU_USUARIOS)
+        opcao = int(input("Informe a opção: "))
+
+        match opcao:
+            case 0:
+                print("Saindo...")
+                break
 
 def listar_usuarios():
     return session.query(Usuario).all()
