4bed4b3baac8383271e0de4f56ddb1000d1815ce
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-09-28 11:41:23 -0300

    Arquivos aula 01 fundamentos web com python

--- modulo03-aplicacoes-web-com-python/20240928-aula01/enquetes/models.py
+++ modulo03-aplicacoes-web-com-python/20240928-aula01/enquetes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Pergunta(models.Model):
+    texto = models.CharField(max_length=200)
+    data_publicacao = models.DateTimeField("Data de publicação")
+
+
+class Opcao(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
