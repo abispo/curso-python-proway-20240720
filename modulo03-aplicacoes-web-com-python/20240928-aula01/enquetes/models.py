b62ca627d7bb08bec48f3b276224f2e086a2e119
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-09-28 12:30:12 -0300

    Arquivos aula 01 aplicações web com python

--- modulo03-aplicacoes-web-com-python/20240928-aula01/enquetes/models.py
+++ modulo03-aplicacoes-web-com-python/20240928-aula01/enquetes/models.py
@@ -5,8 +5,15 @@ class Pergunta(models.Model):
     texto = models.CharField(max_length=200)
     data_publicacao = models.DateTimeField("Data de publicação")
 
+    # Dentro de uma model, podemos alterar as configurações padrão dela declarando a classe Meta. No exemplo abaixo, alteramos o nome padrão que o Django daria para a tabela para tb_perguntas
+    class Meta:
+        db_table = "tb_perguntas"
+
 
 class Opcao(models.Model):
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
     texto = models.CharField(max_length=200)
     votos = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "tb_opcoes"
