36e5bd85bb2d3a64e0a3d86ab86ba1ff38fa9efd
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-10-05 11:55:21 -0300

    Arquivos aula 02 aplicações web com python

--- modulo03-aplicacoes-web-com-python/20241005-aula02/enquetes/views.py
+++ modulo03-aplicacoes-web-com-python/20241005-aula02/enquetes/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 
+from .models import Pergunta
+
 # HTTP -> HyperText Transfer Protocol
 
 # Essa função chamamos de view. Uma view é responsável por retornar uma resposta http válida para o navegador. Podemos criar 2 tipos de views: As function-based-views (Views baseadas em função) e as class-based-views(Views baseadas em classe).
 def index(request):
-    return HttpResponse("Você está na página principal das enquetes.")
+    lista_perguntas = Pergunta.objects.order_by(
+        "-data_publicacao"
+    )
+
+    saida = ", ".join([pergunta.texto for pergunta in lista_perguntas])
+    return HttpResponse(saida)
 
 def detalhes(request, pergunta_id):
     return HttpResponse(f"Você está na página de detalhes da pergunta {pergunta_id}.")
