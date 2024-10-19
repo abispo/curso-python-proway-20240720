f135afe7ddd3fcaa0ddd7fb771d1ead4f4062ba5
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-10-19 12:19:54 -0300

    Arquivos aula 04 aplicações web com Python

--- modulo03-aplicacoes-web-com-python/20241019-aula04/estatisticas/views.py
+++ modulo03-aplicacoes-web-com-python/20241019-aula04/estatisticas/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Count, Avg
+from django.db.models import Count, Avg, Sum
 from django.shortcuts import render
 
 from enquetes.models import Pergunta, Opcao
@@ -13,12 +13,18 @@ def index(request):
     #     numero_de_opcoes=Count("opcao")
     # ).aggregate(media_opcoes=Avg("numero_de_opcoes"))["media_opcoes"]
 
+    # O método annotate cria atributos nos objetos que estão sendo retornados do banco. No caso abaixo, para cada instância da model Pergunta, está sendo criado o atributo num_votos, que irá armazenar a quantidade de votos que as opções de cada pergunta tiveram.
+    lista_de_perguntas_ordenada_pela_quantidade_de_votos = Pergunta.objects.annotate(
+        num_votos=Sum("opcao__votos")
+    ).order_by("-num_votos")[:5]
+
     return render(
         request,
         "estatisticas/index.html",
         context={
             "quantidade_de_perguntas_cadastradas": quantidade_de_perguntas_cadastradas,
             "quantidade_de_opcoes_cadastradas": quantidade_de_opcoes_cadastradas,
-            "media_de_opcoes_por_pergunta": media_de_opcoes_por_pergunta
+            "media_de_opcoes_por_pergunta": media_de_opcoes_por_pergunta,
+            "lista_de_perguntas_ordenada_pela_quantidade_de_votos": lista_de_perguntas_ordenada_pela_quantidade_de_votos
         }
     )
