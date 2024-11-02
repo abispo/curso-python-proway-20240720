2d697ca046258edf998b6864d5a8718e348b6b7f
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-11-02 12:56:14 -0300

    Arquivos aula 06 Aplicações web com Python

--- modulo03-aplicacoes-web-com-python/20241102-aula06/gestao_imoveis/urls.py
+++ modulo03-aplicacoes-web-com-python/20241102-aula06/gestao_imoveis/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include("django.contrib.auth.urls"))
 ] + debug_toolbar_urls()
