65e7cf99b93ca8a90e032b03121e8267a0dfa486
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-11-09 09:59:49 -0300

    Arquivos aula 07 aplicações web com python

--- modulo03-aplicacoes-web-com-python/20241109-aula07/gestao_imoveis/urls.py
+++ modulo03-aplicacoes-web-com-python/20241109-aula07/gestao_imoveis/urls.py
@@ -21,5 +21,6 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contas/', include("django.contrib.auth.urls"))
+    path('contas/', include("django.contrib.auth.urls")),
+    path('registro/', include("registro.urls"))
 ] + debug_toolbar_urls()
--- modulo03-aplicacoes-web-com-python/20241109-aula07/registro/views.py
+++ modulo03-aplicacoes-web-com-python/20241109-aula07/registro/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def pre_registro(request):
+    return render(request, "registro/pre_registro.html")
